1f374dd2c34dc447ee06e0ce6056665ac2fd01d3
eshika <eshika22som2003@gmail.com>
2023-03-01 01:29:43 +0530

    ok

--- hello.py
+++ hello.py
@@ -1,5 +1,19 @@
 # print("hello dada")
-a=5
+a=20
 b=20
 c=a+b
 print (c) 
+print(id(a)) #address of a
+print(id(b)) #address of b
+print(type(a))
+x=5+7j
+print(type(x))
+str1="eshika"
+print(type(str1))
+data=[0,1,5,6] #list
+print(type(data))
+data1=("bapi",2,10,1963) #tuple
+print(type(data1))
+mp={100:"eshika",200:"rina",300:"pronab"}
+print(type(mp))
+print(mp[100])
